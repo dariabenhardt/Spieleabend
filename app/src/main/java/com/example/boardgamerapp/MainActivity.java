29ac74ba51858a133034a9b177bfc0b91f794d0c
Michaela Markovic <michaela.markovic@iubh-fernstudium.de>
2026-05-05 12:32:05 +0200

    fixed functionality of rating button and modified the design of past event cards

--- app/src/main/java/com/example/boardgamerapp/MainActivity.java
+++ app/src/main/java/com/example/boardgamerapp/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button ratingButton = findViewById(R.id.rating_button);
+        ratingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, RateEventActivity.class);
+               startActivity(intent);
+            }
         });
     }
 }
