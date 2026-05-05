3d760798d0ff7f82f13ebac14c5e24de7d8b354b
Michaela Markovic <michaela.markovic@iubh-fernstudium.de>
2026-05-05 13:34:32 +0200

    fixed functionality of second rating button

--- app/src/main/java/com/example/boardgamerapp/MainActivity.java
+++ app/src/main/java/com/example/boardgamerapp/MainActivity.java
@@ -32,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
             }
         });
+
+        Button ratingButton2 = findViewById(R.id.rating_button2);
+        ratingButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RateEventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
